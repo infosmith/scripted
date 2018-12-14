c3031d223befc2487edf89b078abdf558661dcd2
infosmith <dst.dsmith@gmail.com>
2018-12-14 13:43:41 -0500

    refactor: add explicit constraints
    Why:
    
    * Exposing APIs should be deliberate.
    
    This change addresses the need by:
    
    * Inherit from ABC to discourage instantiation.

--- scripted/core/bases.py
+++ scripted/core/bases.py
@@ -1,11 +1,12 @@
 """Entry point of framework."""
+from abc import ABC
 from scripted.core.helpers import Helpers
 
 
 __all__ = ['Controller', 'View']
 
 
-class Base(object):
+class Base(ABC):
     """Base class of all Scripted classes."""
 
     fn = Helpers()
