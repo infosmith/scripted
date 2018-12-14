d0fb2c8710fb3894d7d0f6e74401d40b9e4d7a4e
infosmith <dst.dsmith@gmail.com>
2018-12-14 13:35:56 -0500

    feat: add flattend import hierarchy
    Why:
    
    * Simplify developer experience.
    
    This change addresses the need by:
    
    * Add flattened import hierarchy.

--- scripted/__init__.py
+++ scripted/__init__.py
@@ -1,5 +1,8 @@
-from scripted.core.decorator import Script
 """Top-level Scripted package."""
+from scripted.core import Controller
+from scripted.core import Helpers
+from scripted.core import Script
+from scripted.core import View
 
 __author__ = 'David Smith'
 __email__ = 'infosmith@protonmail.com'
